a0753fba3ed8ed7aeb0c90a1b255558932717b5c
Francisco López <fclopezjurado@gmail.com>
2017-02-27 22:26:50 +0100

    Add new class "Minesweeper" in Main folder to launch the application-

---
+++ Minesweeper/Minesweeper/Main/Minesweeper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using Minesweeper.Views;
+
+namespace Minesweeper.Main
+{
+    static class Minesweeper
+    {
+        private const int BOARD_X           = 5;
+        private const int BOARD_Y           = 5;
+        private const int BOMBS_PERCENTAGE  = 33;
+        private const int FORM_WIDTH        = 640;
+        private const int FORM_HEIGHT       = 679;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new InitBoard());
+        }
+    }
+}
